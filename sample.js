ddca35b9582aed83a47fad448053187b6e24fdda
akshara-aravind <aksharareddydilli1001@gmail.com>
2022-12-17 10:11:32 +0530

    changes done

--- sample.js
+++ sample.js
@@ -1,22 +1,22 @@
-function App(props) {
+function Greetings(props){
     const hr=props.date.getHours();
     return (
+    <div>
+    <h1> Hello {props.name}, </h1>
+    <h1>
+    {hr<12 && <div>Good Morning!</div>}
+    {hr>12 && hr<15 && <div>Good Afternoon!</div>}
+    {hr>15 && <div>Good Evening!</div>}
+    </h1>
+    
+    </div>
+    );
+    }
+    
+  function App() {
+    return (
       <div>
-        Hello, {props.name} 
-        {
-            {hr}<12 && <div> Good Morning</div>
-        }
-        {
-            {hr}>12 && {hr}<15 && <div> Good Afternoon</div>
-        }
-        {
-            {hr}>15 && <div> Good Evening</div>
-        }
+      <Greetings name="sara" date={new Date()}/> 
       </div>
     );
-   }
-  
-  
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  const Greetings = <App name="Sara" date={new Date()} />;
-  root.render(Greetings);
+  }
